c6bbbc6f5366ddeffe2cc8ca096cfb750c2d4750
mjsteger <mjsteger1@gmail.com>
2014-05-27 23:29:00 -0700

    First off commit for setting up my personal files

---
+++ init.el
@@ -0,0 +1,7 @@
+(load "~/dreamacs/dotemacs")
+
+(defvar dreamacs-personal-dir (expand-file-name "personal" "~/.emacs.d/"))
+
+(when (file-exists-p dreamacs-personal-dir)
+  (message "Loading personal configuration files in %s..." dreamacs-personal-dir)
+  (mapc 'load (directory-files dreamacs-personal-dir 't "^[^#].*el$")))
