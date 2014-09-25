b71b9617e88f7e7d7b2745318e1d2a33e9cd623a
mjsteger <mjsteger1@gmail.com>
2014-09-25 05:46:48 -0700

    Only use linum mode when goto-line is being used(linum is a bit of a hog)

--- personal/efuncs.el
+++ personal/efuncs.el
@@ -12,3 +12,13 @@
                 (lambda ()
                   (interactive)
                   (join-line -1)))
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
+(global-set-key (kbd "M-g") 'goto-line-with-feedback)
