3c117b1521769981c7a1bef1db7cc0f35e198911
mjsteger <mjsteger1@gmail.com>
2014-09-25 05:39:49 -0700

    remove annoying comment, add function from whattheemacsd

--- personal/efuncs.el
+++ personal/efuncs.el
@@ -1,5 +1,3 @@
-;; All me, baby. Try it out. If theres a region, it'll kill that,
-;; otherwise it'll just kill the word behind.
 (defun backward-kill-word-or-kill-region (&optional arg)
   (interactive "P")
   (if (region-active-p)
@@ -9,3 +7,8 @@
       (subword-backward-kill arg))))
 
 (global-set-key (kbd "C-w") 'backward-kill-word-or-kill-region)
+
+(global-set-key (kbd "M-j")
+                (lambda ()
+                  (interactive)
+                  (join-line -1)))
